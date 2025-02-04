83e198e942be67fe44da763b1d4caa3aeed6bdf1
EL BAHTOURI <elbahtouri@192.168.1.8>
2025-02-04 01:38:01 +0100

    add main.cpp

---
+++ C00/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include "phonebook.hpp"
+
+int main()
+{
+	int			i;
+	phonebook	div;
+	st	input;
+
+	input = "";
+	i = 0;
+	while (1)
+	{
+		std::cout << BLUE "COMMANDS : " RESET << CYAN "ADD | SEARCH | EXIT " RESET << std::endl;
+		std::cout << RED "Enter one : " RESET;
+		if (!std::getline(std::cin, input))
+			return (0);
+		if (!input.compare("EXIT"))
+			return (0);
+		if (!input.compare("ADD"))
+			i = add_contact(&div, i);
+		if (i > 7)
+			i = 0;
+		if (!input.compare("SEARCH"))
+			search_contact(&div);
+	}
+	return (0);
+}
